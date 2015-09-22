7dd6c567e8c036920375b2593cba4bcbac20ca41
Kyle Pratuch & Charles Moss <student@epicodus.com>
2015-09-22 11:34:10 -0700

    add permission slip toggle

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,22 +1,12 @@
-studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
-    $scope.students = [];
-    $scope.grads = [];
-    $scope.addStudent = function() {
-        $scope.students.push({
-            name: $scope.studentName
-        });
-        $scope.studentName = null;
-    };
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory) {
+    $scope.students = StudentsFactory.students;
+    $scope.StudentsFactory = StudentsFactory;    
 
-    $scope.deleteStudent = function(student) {
-        var index = $scope.students.indexOf(student);
-        $scope.students.splice(index, 1);
-        console.log(index);
-    };
+    // $scope.grads = [];
 
-    $scope.graduateStudent = function(student) {
-        $scope.grads.push(student);
-        $scope.deleteStudent(student);
-    };
+    // $scope.graduateStudent = function(student) {
+    //     $scope.grads.push(student);
+    //     $scope.deleteStudent(student);
+    // };
 
 });
